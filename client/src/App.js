7c10a8483ec31f423186653b5827c1071aef78de
namita.shenai@gmail.com <namita.shenai@gmail.com>
2020-07-13 22:26:23 -0700

    changed the UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,13 @@
 import React from 'react';
+import logo from './logo.svg';
 import SendMails from "./components/SendMails";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-       
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+       <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
       <SendMails />
     </div>
   );
--- server.js
+++ server.js
@@ -20,9 +20,15 @@ app.post("/api/sendMail", (req,res) =>{
 })
 
 //TESTING PURPOSE 
-sendEmail(process.env.REACT_APP_GMAIL, process.env.REACT_APP_PASSWORD, "hello");
-sendEmail(process.env.REACT_APP_GMAIL, process.env.REACT_APP_PASSWORD, "thanks");
-sendEmail(process.env.REACT_APP_GMAIL, process.env.REACT_APP_PASSWORD, "");
+for ( var i =0 ; i <= 25  ; i++){
+  sendEmail(process.env.REACT_APP_GMAIL, "Mr.X "  , "hello");
+}
+
+// for ( var i =0 ; i <= 5 ; i++){
+  sendEmail(process.env.REACT_APP_GMAIL, "Ms. Cindrella - ", "thanks");
+// }
+
+sendEmail(process.env.REACT_APP_GMAIL, "Mr. Nobody", "");
 
 app.listen(PORT, () =>{
     console.log("App listening on: http://localhost:" + PORT);
